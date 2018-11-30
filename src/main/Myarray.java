e3c2e4074c91eb4e75c9c87140f21eada100e5fb
vbobrik <v.bobrik@kino-mo.com>
2018-11-30 19:57:41 +0300

    init commit

---
+++ src/main/Myarray.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Myarray {
+    private int[] myArray = new int[200];
+
+    public int[] getMyArray() {
+      return  this.myArray;
+    }
+    public void setMyArray(int[] newArray) {
+        this.myArray = newArray;
+    }
+//-------ADD---elem-----
+    public void addElem(int numbers) {
+        int i;
+        for(i = 0; i <= this.myArray.length; i ++) {
+            this.myArray[i] =
+        }
+    }
+
+    Random rand = new Random();
+
+    int n = rand.nextInt(50) + 1;
+
+
+
+}
---
+++ src/main/Random.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Random {
+
+    Myarray rand = new Myarray();
+     int randomNum = rand.nextInt;
+}
